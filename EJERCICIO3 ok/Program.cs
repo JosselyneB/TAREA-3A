1c7eb0c3e15632bf820bbde1c2d0d661d50a9e3f
JosselyneB <118408836+JosselyneB@users.noreply.github.com>
2022-11-20 15:42:00 -0500

    Add files via upload

---
+++ EJERCICIO3 ok/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class NewBaseType
+{
+    static void Main(string[] args){
+
+        Console.WriteLine("Ingrese el primer numero");
+        int num1=int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese el segundo numero");
+        int num2=int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese el tercer numero");
+        int num3=int.Parse(Console.ReadLine());
+
+        if((num1>num2 && num1>num3)){
+             Console.WriteLine("EL NUMERO MAYOR ES: " + num1);
+        }
+        else if(num2>num1 && num2 > num3){
+                 Console.WriteLine("ES NUMERO MAYOR ES: " + num2);
+          }else
+                   Console.WriteLine("EL NUMERO MAYOR ES: " + num3);
+
+    }
+
+}
