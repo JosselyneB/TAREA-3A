bbb5336ed50249891082fae4efa1bdf6b3d02eb4
JosselyneB <118408836+JosselyneB@users.noreply.github.com>
2022-11-20 15:47:17 -0500

    Add files via upload

---
+++ EJERCICIO8 ok/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program{
+
+     static void Main (string[]args){
+        int multiploTres=0;
+        int contador=0;
+        
+       do {
+        Console.WriteLine("LOS MULTIPLOS DE 3 SON: " + multiploTres);
+        multiploTres=multiploTres+3;
+        contador=contador+1;
+       } while (multiploTres<300);
+
+    }
+}
