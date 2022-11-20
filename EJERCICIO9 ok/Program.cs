22671c7a835ce1701be00965d875bd82a3798ab9
JosselyneB <118408836+JosselyneB@users.noreply.github.com>
2022-11-20 15:47:50 -0500

    Add files via upload

---
+++ EJERCICIO9 ok/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program{
+    static void main (string [] args){
+      int num;
+      int suma=0;
+      int resultado=0;
+
+
+      for (int n=8; n>=0; n--){
+        Console.WriteLine("INTRODUCIR UN NUMERO");
+        num = int.Parse(Console.ReadLine());
+
+        resultado=suma/8;
+
+        Console.WriteLine("LA MEDIA ARITMETICA ES: " + resultado);
+      }
+
+    }
+      
+}
+
