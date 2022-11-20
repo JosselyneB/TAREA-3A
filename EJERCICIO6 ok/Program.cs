d069caf415825ad28062a152a41d784ec86607ef
JosselyneB <118408836+JosselyneB@users.noreply.github.com>
2022-11-20 15:45:48 -0500

    Add files via upload

---
+++ EJERCICIO6 ok/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program {
+    static void Main (string [] args){
+        int suma=0;
+
+        for(int i=10000; i>=0; i--){
+            Console.WriteLine("  " + i);
+            suma=suma + i;
+        }
+        Console.WriteLine("LA SUMA ES: " + suma);
+
+    }
+}
