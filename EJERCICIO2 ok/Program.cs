4ca5b4e27e64604b5422bd8e3865057ff8664fc3
JosselyneB <118408836+JosselyneB@users.noreply.github.com>
2022-11-20 15:39:36 -0500

    Add files via upload

---
+++ EJERCICIO2 ok/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    public static void Main(string[] args){
+      int num=0;
+       int par, impar;
+
+
+        Console.WriteLine("ingrese un numero");
+        num=int.Parse(Console.ReadLine());
+        if(num%2==0)
+        {
+          Console.WriteLine("par");
+        }
+        else
+        {
+         Console.WriteLine("impar");
+        }
+    
+        }
+}
