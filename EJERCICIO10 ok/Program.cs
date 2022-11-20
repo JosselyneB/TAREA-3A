68f8262139522c49b0ea2f0afb2c1159433a7384
JosselyneB <118408836+JosselyneB@users.noreply.github.com>
2022-11-20 15:48:50 -0500

    Add files via upload

---
+++ EJERCICIO10 ok/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program{
+    
+        static void Main(string[] args)
+        {
+            int num;
+
+            do
+            {
+                Console.Write("DIGITE UN NUMERO ENTERO: ");
+                num = int.Parse(Console.ReadLine());
+            } while (num < 0);
+            factorial(num);
+           
+        }
+       
+        static void factorial(int n)
+        {
+            int fact=1;
+            for (int i = 1; i <= n; i++)
+            {
+                fact *= i;
+            }
+            Console.WriteLine("EL FACTORIAL DE " + n + " ES: " + fact);
+        }
+    }
+
+
